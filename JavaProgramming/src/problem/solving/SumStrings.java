7ce32a1be0bb5791626a2d86a94f9925299f10cc
Lawanu Borthakur <lawanu1@gmail.com>
2021-01-05 20:48:08 +0530

    Program for adding two strings.

---
+++ JavaProgramming/src/problem/solving/SumStrings.java
@@ -0,0 +1,13 @@
+package problem.solving;
+
+public class SumStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="555";
+String s2="111";
+int sum=Integer.parseInt(s1)+Integer.parseInt(s2);
+System.out.println(String.valueOf(sum));
+	}
+
+}
